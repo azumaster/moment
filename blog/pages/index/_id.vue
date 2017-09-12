e22537c1437821ed59fec21308710da100695e58
Azumar <azumar@qq.com>
2017-09-12 15:46:10 +0800

    新增：文章详情的界面

--- blog/pages/index/_id.vue
+++ blog/pages/index/_id.vue
@@ -1,3 +1,37 @@
 <template>
-  <h1>文章详情</h1>
+  <section id="blog-detail">
+    <div class="blog-detail-title">
+      <div class="title-word">
+        <h1>标题标题标题</h1>
+        <p>2017.09.12 / 前端开发 / Azumar</p>
+      </div>
+      <div class="title-see">1719</div>
+    </div>
+    <div class="blog-detail-content">
+      <p>的首发式地方发生发射点速度发送方说了看见了</p>
+    </div>
+    <div class="blog-detail-operate">
+      <div class="operate-next">
+        <p><i class="icon prev"></i>上一篇：XXXXX</p>
+        <p>下一篇：XXXXX<i class="icon next"></i></p>
+      </div>
+    </div>
+    <div class="blog-detail-replay">
+      <div class="replay-list">
+        <div class="replay-item">
+          <div class="replay-name">王小明：</div>
+          <div class="replay-content">啊对斯洛伐克达拉斯发的角度考虑是发进来咯加分；链式附近的老师放假了</div>
+          <div class="replay-time">2017.09.12 15:33:12</div>
+        </div>
+      </div>
+      <div class="replaying">
+        <form class="form">
+          <div class="form-item require"><input id="name" type="text" placeholder="请输入用户名（必填）"><label for="name">用户名（必填）</label></div>
+          <div class="form-item require"><input id="email" type="email" placeholder="请输入Email（必填，非公开）"><label for="email">E-mail（必填，非公开）</label></div>
+          <div class="form-item require"><textarea id="review" placeholder="请输入评论内容"></textarea><label for="review">评论内容（必填）</label></div>
+          <div class="form-item"><button type="button">提交评论</button></div>
+        </form>
+      </div>
+    </div>
+  </section>
 </template>
